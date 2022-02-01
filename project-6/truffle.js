1adef19a90fa16a17da5cd3e8c00618dc9f09eee
Pranay Sarkar <pranaysarkar22@gmail.com>
2022-02-01 21:09:09 +0100

    na: truffle deployment configs added [Pranay Sarkar] [V] [Pranay Sarkar] [V]

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,19 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = "8e6eeecb5a854126867b2025f751ef6a";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
     }
   },
   compilers: {
@@ -12,3 +22,5 @@ module.exports = {
     }
    }
 };
+
+// truffle migrate --reset --network rinkeby
